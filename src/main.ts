0ef788d9e34c549e431e0e0c2446a9059448a0c3
Ashlyn Knox <ashlyn@lilyx.io>
2022-07-14 10:01:19 -0600

    removed inkline

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { Inkline, components } from "@inkline/inkline";
 
-import "@/assets/inkline.scss"
 import "@/assets/tailwind.css";
 
 const app = createApp(App);
-
-app.use(Inkline, { 
-  components
-});
 
 app.use(router);
 
